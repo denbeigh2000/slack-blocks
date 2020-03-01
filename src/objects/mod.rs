9dc284e45726323710337f78af6c40281647fd59
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2020-03-01 11:55:11 -0800

    improve api of/unit test object enum type

--- src/objects/mod.rs
+++ src/objects/mod.rs
@@ -4,7 +4,7 @@ mod option;
 pub use crate::objects::text::{FormattingType, Text, TextBuilder};
 pub use crate::objects::option::{OptionInput, OptionInputBuilder, OptionInputGroup};
 
-use serde::Serialize;
+use serde::{Serialize, Serializer};
 
 // Reference: https://api.slack.com/reference/block-kit/composition-objects#text
 
@@ -39,6 +39,44 @@ pub enum Object {
     OptionInputGroup(OptionInputGroup),
 }
 
+impl Into<Object> for Text {
+    fn into(self) -> Object {
+        Object::Text(self)
+    }
+}
+
+impl Into<Object> for ConfirmationDialog {
+    fn into(self) -> Object {
+        Object::ConfirmationDialog(self)
+    }
+}
+
+impl Into<Object> for OptionInput {
+    fn into(self) -> Object {
+        Object::Option(self)
+    }
+}
+
+impl Into<Object> for OptionInputGroup {
+    fn into(self) -> Object {
+        Object::OptionInputGroup(self)
+    }
+}
+
+impl Serialize for Object {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        match self {
+            Object::Text(e) => e.serialize(serializer),
+            Object::ConfirmationDialog(e) => e.serialize(serializer),
+            Object::Option(e) => e.serialize(serializer),
+            Object::OptionInputGroup(e) => e.serialize(serializer),
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -51,6 +89,22 @@ mod test {
             "Yes",
             "No",
         );
+        let json = serde_json::to_string(&dialog).unwrap();
+        assert_eq!(
+            json.as_str(),
+            r#"{"title":{"type":"plain_text","text":"Confirm?"},"text":{"type":"mrkdwn","text":"testing"},"confirm":{"type":"plain_text","text":"Yes"},"deny":{"type":"plain_text","text":"No"}}"#
+        );
+    }
+
+    #[test]
+    fn object_serialize() {
+        let dialog: Object = ConfirmationDialog::new(
+            "Confirm?",
+            Text::builder(FormattingType::Markdown, "testing").build(),
+            "Yes",
+            "No",
+        ).into();
+
         let json = serde_json::to_string(&dialog).unwrap();
         assert_eq!(
             json.as_str(),
