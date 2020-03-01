1e87a02769109099d3166fd51d2f3d4217a7434e
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2020-02-29 23:16:16 -0800

    removed derive_builder from views

--- src/views.rs
+++ src/views.rs
@@ -1,12 +1,24 @@
 use crate::surfaces::*;
 
-use derive_builder::Builder;
-
-#[builder(setter(into), pattern = "owned")]
-#[derive(Builder)]
 pub struct View<T: ViewType> {
     pub payload: T,
     pub private_metadata: Option<String>,
+}
+
+impl<T: ViewType> View<T> {
+    pub fn new(payload: T) -> View<T> {
+        View {
+            payload,
+            private_metadata: None,
+        }
+    }
+
+    pub fn new_with_metadata(payload: T, private_metadata: String) -> View<T> {
+        View {
+            payload,
+            private_metadata: Some(private_metadata),
+        }
+    }
 }
 
 pub trait ViewType {}
